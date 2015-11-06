18053fa3303089cea7ea47d0d3abd7fd4d9480a8
Tofik <i92596@IPVAPW7D-A245.iso.com>
2015-11-06 12:33:05 -0500

    Initial commit

---
+++ os.java
@@ -0,0 +1,20 @@
+
+public class os {
+
+	public static void startup() {
+		System.out.println("working");
+		//sos.ontrace();
+	}
+	
+	public static void main(String[] args) {
+		System.out.println("wokring");
+		
+	}
+	
+	public static void Crint (int []a, int []p)   {}
+	public static void Dskint (int []a, int []p)  {}
+	public static void Drmint (int []a, int []p)  {}
+	public static void Tro (int []a, int []p)     {}
+	public static void Svc (int []a, int []p)     {}
+
+}
