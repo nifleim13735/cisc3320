d511974d1fe029824e1d4216df8915e81153274a
Viachaslau Kalatsei <iSlavander@gmail.com>
2015-12-06 14:21:36 -0500

    Update PCB.java

--- PCB.java
+++ PCB.java
@@ -1,105 +1,96 @@
 
 public class PCB {
+	public final static String CREATED = "CREATED";
+	public final static String READY = "READY";
+	public final static String RUNNING = "RUNNING";
+	public final static String WAITING = "WAITING";
+	public final static String TERMINATED = "TERMINATED";
+	
 	String status;
 	int jobNumber;
 	int priority;
 	int jobSize;
 	int maxCpuTime;
-	int timeCPU_Used;
 	int currentTime;
-	int processingTime;
 	int startingAddress;
-	int address;
-	boolean needsIO;
-	boolean termination;
-	
-
-
+	int cpuTimeUsed;
+	int timeSlice;
+	int outstandingIoRequests;
+	Boolean isBlocked;
+	Boolean markedForTermination;
 
 	PCB(int jobNumber, int priority, int jobSize, int maxCpuTime, int currentTime, int startingAddress) {
-		
-		this.status = "CREATED";
+		this.status = CREATED;
 		this.jobNumber = jobNumber;
 		this.priority= priority;
 		this.jobSize= jobSize;
 		this.maxCpuTime= maxCpuTime;
 		this.currentTime = currentTime;
 		this.startingAddress= startingAddress;
-		needsIO=false;
-		timeCPU_Used=0;
-		address = -1;
-		termination=false;
+		this.cpuTimeUsed = 0;
+		this.outstandingIoRequests = 0;
+		this.isBlocked = false;
+		this.markedForTermination = false;
+		this.timeSlice = 0;
+		this.setTimeSlice();
+	}
+
+	public void incrementCpuTimeUsed(int time){
+		if (!(this.isBlocked)){
+			this.cpuTimeUsed += time;
+		}
+		setTimeSlice();
+	}
+
+	public void ioRequested() {
+		this.outstandingIoRequests += 1;				
+		os.ioQueue.add(this);  // job needs to do I/O
+	}
+
+	public void ioRequestCompleted() {
+		this.outstandingIoRequests -= 1;
+		os.ioQueue.remove(this);
+		if (this.outstandingIoRequests == 0 ){
+			System.out.println("All requests for job " + this.jobNumber + " completed");
+			if (this.isBlocked) { this.unblockJob(); }
+			if (this.markedForTermination){
+				Scheduler.terminateJob(this);
+			} else {
+				this.status= READY;
+				os.readyQueue.add(this);
+			}
+		}
+	}
+
+	public void blockJob() {
+		this.status = WAITING;
+		this.isBlocked = true;
 	}
 	
-	public void ProcessingTime(int processingTime){
-        this.processingTime = processingTime;
-    }
-	
-	public void ProcessTime(int currentTime)
-	{
-        timeCPU_Used = timeCPU_Used + currentTime - processingTime;
-    }
-	
-	public int MaxCpuTime()
-	{
-		return maxCpuTime;
+	public void unblockJob() {
+		this.isBlocked = false;
 	}
-	
-	public int TimeCPU_Used()
-	{
-		return timeCPU_Used;
+
+	public void setTimeSlice(){
+		this.timeSlice = (this.cpuTimeRemaining() < os.TIMESLICE) ? this.cpuTimeRemaining() : os.TIMESLICE;
 	}
-	
-	public boolean NeedsIO_Status()
-	{
-		return needsIO;
+
+
+	public int cpuTimeRemaining()  {
+		return maxCpuTime - cpuTimeUsed;
 	}
-	
-	public void wantsIO_On()
-	{
-		needsIO=true;
+	public int getJobNumber()  {
+		return this.jobNumber;
+	// }
+	public void markForTermination() {
+		this.markedForTermination = true;
 	}
-	
-	public void wantsIO_Off()
-	{
-		needsIO=false;
-	}
-	
-	public boolean Termination_Status()
-	{
-		return termination;
-	}
-	
-	public void Termination_On()
-	{
-		termination=true;
-	}
-	
-	public void Termination_Off()
-	{
-		termination=false;
-	}
-	
-	public int getAddress()
-	{
-        return address;
-    }
-	
-	public void setAddress(int address)
-	{
-        this.address = address;
-    }
-	
-	public int getjobSize()
-	{
-        return jobSize;
-    }
-	
-	
 	
 	public String toString() {
-		return "Job#: " + this.jobNumber + ", priority: " + this.priority + ", jobSize: " + this.jobSize + ", maxCpuTime: " + this.maxCpuTime + ", currentTime: " + this.currentTime
-		+ ", startingAddress: " + this.startingAddress;
+		return " Job#: " + this.jobNumber + ", status:" + this.status + ", priority: " + this.priority + ", jobSize: " + this.jobSize + ", maxCpuTime: " + this.maxCpuTime 
+				+ ", cpu time used: " + this.cpuTimeUsed + ", cpu time remaining: " + this.cpuTimeRemaining() 
+				+ ", timeSlice on next tick: " + this.timeSlice + ", outstanding I/O requests: " + this.outstandingIoRequests
+				+ ", startingAddress: " + this.startingAddress +"\n";
 	}
 
 }
